18bf557563da5570b87d56fd8701a4045965164d
ayannauhuru <132233253+ayannauhuru@users.noreply.github.com>
2023-05-04 15:03:17 -0400

    Add files via upload

---
+++ Website Pages/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home</title>
+<link rel="stylesheet" href="style.css">
+</head>
+ 
+<body>
+     
+    <!-- Header Section -->
+    <header>
+        <div class= "head1">Berlin</div>
+    </header>
+     
+    <!-- Horizontal Menu Navigation Bar -->
+    <div class= "menu">
+        <a href= "index.php">Home</a>
+        <a href= "weather.php">Weather</a>
+        <a href= "covid_info.php">COVID-19 Information</a>
+        <a href= "museums.php">Museums</a>
+        <a href= "stadium_reviews.php">Stadium Reviews</a>
+        <a href= "translator.php">Translator</a>
+    </div>
+     
+    <!-- Body section -->
+    <div class = "body_sec">
+        <section id= "Content">
+            <h3>Berlin</h3>
+            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Nunc id cursus metus aliquam eleifend mi in nulla. Lobortis feugiat vivamus at augue eget arcu. Mauris sit amet massa vitae.
+            Condimentum vitae sapien pellentesque habitant morbi. Ornare lectus sit amet est placerat in. Mi sit amet mauris commodo quis.
+            Duis convallis convallis tellus id interdum velit laoreet id. Urna nunc id cursus metus.</p>
+            
+            <p> Fames ac turpis egestas integer eget aliquet nibh praesent. Diam ut venenatis tellus in. 
+            Sed vulputate mi sit amet mauris. Habitasse platea dictumst quisque sagittis purus. 
+            Sit amet volutpat consequat mauris nunc congue nisi vitae. Ipsum faucibus vitae aliquet nec ullamcorper sit. 
+            Eget felis eget nunc lobortis mattis aliquam faucibus purus. Tortor at auctor urna nunc id cursus. 
+            Fringilla ut morbi tincidunt augue. </p>
+        </section>
+    </div>
+     
+   <!-- Footer Section -->
+   <div class="footer">
+      <p>Group 6 - Computing and Informatics Capstone - Spring 2022</p>
+    </div>
+    
+</body>
+</html>      
---
+++ Website Pages/museums.php
@@ -0,0 +1,58 @@
+<html>
+    <head>
+<link rel="stylesheet" href="style.css">
+</head>
+
+
+<body>
+
+    <!-- Header Section -->
+    <header>
+        <div class= "head1">Museums</div>
+    </header>
+     
+    <!-- Horizontal Menu Navigation Bar -->
+    <div class= "menu">
+        <a href= "index.php">Home</a>
+        <a href= "weather.php">Weather</a>
+        <a href= "covid_info.php">COVID-19 Information</a>
+        <a href= "museums.php">Museums</a>
+        <a href= "stadium_reviews.php">Stadium Reviews</a>
+        <a href= "translator.php">Translator</a>
+    </div>
+
+<?php
+
+$servername = "sql5.freemysqlhosting.net";
+$username = "";
+$password = "";
+$dbname = '';
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT row_names, name, formatted_address, rating, user_ratings_total FROM BerlinMuseums";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo $row["row_names"]. ": " . $row["name"]. " " . $row["formatted_address"]. " " . $row["rating"]. " "
+. $row["user_ratings_total"]. "<br>". "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+$conn->close();
+
+?>
+
+</body>
+
+</html>
